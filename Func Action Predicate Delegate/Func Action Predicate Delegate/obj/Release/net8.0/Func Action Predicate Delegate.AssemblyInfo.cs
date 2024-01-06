68da328a21717503faf4c26e159da0f60fa0defe
Rayhan Chowdhury <rayhan.chowdhury599@gmail.com>
2024-01-06 23:58:49 +0600

    Delegates - Asynchronous Method Calls (+)

--- Func Action Predicate Delegate/Func Action Predicate Delegate/obj/Release/net8.0/Func Action Predicate Delegate.AssemblyInfo.cs
+++ Func Action Predicate Delegate/Func Action Predicate Delegate/obj/Release/net8.0/Func Action Predicate Delegate.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Func Action Predicate Delegate")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Func Action Predicate Delegate")]
